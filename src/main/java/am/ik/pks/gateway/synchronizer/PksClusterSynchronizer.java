6c724a8d7965645a1d90c06667e4982077531fd5
Toshiaki Maki <tmaki@pivotal.io>
2018-06-23 19:01:20 +0900

    Add refreshing

--- src/main/java/am/ik/pks/gateway/synchronizer/PksClusterSynchronizer.java
+++ src/main/java/am/ik/pks/gateway/synchronizer/PksClusterSynchronizer.java
@@ -1,30 +1,36 @@
 package am.ik.pks.gateway.synchronizer;
 
-import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
-
+import am.ik.pks.gateway.client.PksApiClient;
+import am.ik.pks.gateway.client.PksCluster;
+import org.springframework.cloud.gateway.event.RefreshRoutesEvent;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import am.ik.pks.gateway.client.PksApiClient;
-import am.ik.pks.gateway.client.PksCluster;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Component
 public class PksClusterSynchronizer {
-	private final PksApiClient pksApiClient;
-	private final AtomicReference<List<PksCluster>> clusters = new AtomicReference<>();
+    private final PksApiClient pksApiClient;
+    private final AtomicReference<List<PksCluster>> clusters = new AtomicReference<>();
+    private final ApplicationEventPublisher publisher;
 
-	public PksClusterSynchronizer(PksApiClient pksApiClient) {
-		this.pksApiClient = pksApiClient;
-	}
 
-	@Scheduled(fixedDelayString = "${pks.cluster.synchronize.interval:60000}", initialDelay = 0)
-	public void sync() {
-		this.pksApiClient.getClusters().collectList().doOnNext(this.clusters::set)
-				.subscribe();
-	}
+    public PksClusterSynchronizer(PksApiClient pksApiClient, ApplicationEventPublisher publisher) {
+        this.pksApiClient = pksApiClient;
+        this.publisher = publisher;
+    }
 
-	public List<PksCluster> getClusters() {
-		return clusters.get();
-	}
+    @Scheduled(fixedDelayString = "${pks.cluster.synchronize.interval:30000}", initialDelay = 0)
+    public void sync() {
+        this.pksApiClient.getClusters().collectList()
+                .doOnNext(this.clusters::set)
+                .doFinally(x -> this.publisher.publishEvent(new RefreshRoutesEvent(this)))
+                .subscribe();
+    }
+
+    public List<PksCluster> getClusters() {
+        return clusters.get();
+    }
 }
